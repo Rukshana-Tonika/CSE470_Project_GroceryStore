bd973edf1b937bd2d384d36a640ddee47167fe5d
tonika <rukshanaamintonika@gmail.com>
2022-08-23 22:20:51 +0600

    final

--- E-shop/resources/views/home/userpage.blade.php
+++ E-shop/resources/views/home/userpage.blade.php
@@ -10,7 +10,7 @@
       <meta name="keywords" content="" />
       <meta name="description" content="" />
       <meta name="author" content="" />
-      <link rel="shortcut icon" href="images/Gro_logo.jpeg" type="">
+      <link rel="shortcut icon" href="images/cover.png" type="">
       <title>Online Grocery Store</title>
       <!-- bootstrap core css -->
       <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
